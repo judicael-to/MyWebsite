f15467da0f05139115a00d01fe1407b70f18833e
judicael.to <judicael.to@universite-paris-saclay.fr>
2025-05-15 22:23:36 +0200

    Update styles

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,7 @@
 export const LINKS = {
 	github: 'https://github.com/judicael-to',
 	discord: 'https://discordapp.com/users/',
+	linkedin: 'https://www.linkedin.com/in/judicael-to-274037353/',
 };
 
 export const loaderAnimation = [
