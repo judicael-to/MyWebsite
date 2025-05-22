5a68f27be0aeef23a279bcefd1dab1fe6f3b93c5
judicael.to <judicael.to@universite-paris-saclay.fr>
2025-05-22 23:21:05 +0200

    #4 Solve server problems

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://judicaelto.com/',
+	site: 'https://judicaelto.fr/',
 	integrations: [
 		sitemap(),
 		robotsTxt({
